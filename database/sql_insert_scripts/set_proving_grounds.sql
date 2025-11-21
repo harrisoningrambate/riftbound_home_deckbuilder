54c38287000abaa2403ae241fe87086f67017eba
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2025-11-21 14:11:22 -0800

    i'm a dumbass who lost the original db create script

--- database/sql_insert_scripts/set_proving_grounds.sql
+++ database/sql_insert_scripts/set_proving_grounds.sql
@@ -1,23 +1,133 @@
 -- card_id = 1
-INSERT INTO card (set_name, set_number, card_name, type, rarity, energy)
-VALUES ('proving grounds', '001', 'annie fiery', 'champion unit', 'epic', 5);
+INSERT INTO card VALUES (1, 'proving grounds', '001', 'annie fiery', 'champion unit', 'epic', 5);
 INSERT INTO card_domain VALUES (1, 'fury');
 INSERT INTO card_tag VALUES (1, 'annie');
 INSERT INTO card_tag VALUES (1, 'noxus');
 
 -- card_id = 2
-INSERT INTO card (set_name, set_number, card_name, type, rarity, energy)
-VALUES ('proving grounds', '002', 'firestorm', 'spell', 'uncommon', 6);
+INSERT INTO card VALUES (2, 'proving grounds', '002', 'firestorm', 'spell', 'uncommon', 6);
 INSERT INTO card_domain VALUES (2, 'fury');
 
 -- card_id = 3
-INSERT INTO card (set_name, set_number, card_name, type, rarity, energy)
-VALUES ('proving grounds', '003', 'incinerate', 'spell', 'common', 2);
+INSERT INTO card VALUES (3, 'proving grounds', '003', 'incinerate', 'spell', 'common', 2);
 INSERT INTO card_domain VALUES (3, 'fury');
 
 -- card_id = 4
-INSERT INTO card (set_name, set_number, card_name, type, rarity, energy)
-VALUES ('proving grounds', '004', 'master yi meditative', 'champion unit', 'rare', 5);
+INSERT INTO card VALUES (4, 'proving grounds', '004', 'master yi meditative', 'champion unit', 'rare', 5);
 INSERT INTO card_domain VALUES (4, 'calm');
 INSERT INTO card_tag VALUES (4, 'master yi');
 INSERT INTO card_tag VALUES (4, 'ionia');
+
+-- card_id 5
+INSERT INTO card VALUES (5, 'proving grounds', '005', 'zephyr sage', 'unit', 'uncommon', 6);
+INSERT INTO card_domain VALUES (5, 'calm');
+INSERT INTO card_tag VALUES (5, 'ionia');
+INSERT INTO card_tag VALUES (5, 'bird');
+
+-- card_id 6
+INSERT INTO card VALUES (6, 'proving grounds', '006', 'lux illuminated', 'champion unit', 'rare', 6);
+INSERT INTO card_domain VALUES (6, 'mind');
+INSERT INTO card_tag VALUES (6, 'lux');
+INSERT INTO card_tag VALUES (6, 'demacia');
+
+-- card_id 7
+INSERT INTO card VALUES (7, 'proving grounds', '007', 'garen rugged', 'champion unit', 'rare', 6);
+INSERT INTO card_domain VALUES (7, 'body');
+INSERT INTO card_tag VALUES (7, 'garen');
+INSERT INTO card_tag VALUES (7, 'demacia');
+INSERT INTO card_tag VALUES (7, 'elite');
+
+-- card_id 8
+INSERT INTO card VALUES (8, 'proving grounds', '008', "gentleman's duel", 'spell', 'common', 6);
+INSERT INTO card_domain VALUES (8, 'body');
+
+-- card_id 9
+INSERT INTO card VALUES (9, 'proving grounds', '009', 'master yi honed', 'champion unit', 'epic', 7);
+INSERT INTO card_domain VALUES (9, 'body');
+INSERT INTO card_tag VALUES (9, 'master yi');
+INSERT INTO card_tag VALUES (9, 'ionia');
+
+-- card_id 10
+INSERT INTO card VALUES (10, 'proving grounds', '010', 'annie stubborn', 'champion unit', 'rare', 4);
+INSERT INTO card_domain VALUES (10, 'chaos');
+INSERT INTO card_tag VALUES (10, 'annie');
+INSERT INTO card_tag VALUES (10, 'noxus');
+
+-- card_id 11
+INSERT INTO card VALUES (11, 'proving grounds', '011', 'flash', 'spell', 'common', 2);
+INSERT INTO card_domain VALUES (11, 'chaos');
+
+-- card_id 12
+INSERT INTO card VALUES (12, 'proving grounds', '012', 'blast of power', 'spell', 'common', 6);
+INSERT INTO card_domain VALUES (12, 'order');
+
+-- card_id 13
+INSERT INTO card VALUES (13, 'proving grounds', '013', 'garen commander', 'champion unit', 'epic', 6);
+INSERT INTO card_domain VALUES (13, 'order');
+INSERT INTO card_tag VALUES (13, 'elite');
+INSERT INTO card_tag VALUES (13, 'garen');
+INSERT INTO card_tag VALUES (13, 'demacia');
+
+-- card_id 14
+INSERT INTO card VALUES (14, 'proving grounds', '014', 'lux crownguard', 'champion unit', 'epic', 4);
+INSERT INTO card_domain VALUES (14, 'order');
+INSERT INTO card_tag VALUES (14, 'lux');
+INSERT INTO card_tag VALUES (14, 'demacia');
+
+-- card_id 15
+INSERT INTO card VALUES (15, 'proving grounds', '015', 'recruit the vanguard', 'spell', 'uncommon', 6);
+INSERT INTO card_domain VALUES (15, 'order');
+
+-- card_id 16
+INSERT INTO card VALUES (16, 'proving grounds', '016', 'vanguard attendant', 'unit', 'common', 6);
+INSERT INTO card_domain VALUES (16, 'order');
+INSERT INTO card_tag VALUES (16, 'elite');
+INSERT INTO card_tag VALUES (16, 'demacia');
+
+-- card_id 17
+INSERT INTO card VALUES (17, 'proving grounds', '017', 'dark child-starter', 'legend', 'rare', NULL);
+INSERT INTO card_domain VALUES (17, 'fury');
+INSERT INTO card_domain VALUES (17, 'chaos');
+INSERT INTO card_tag VALUES (17, 'annie');
+
+-- card_id 18
+INSERT INTO card VALUES (18, 'proving grounds', '018', 'tibbers', 'signature unit', 'epic', 8);
+INSERT INTO card_domain VALUES (18, 'fury');
+INSERT INTO card_domain VALUES (18, 'chaos');
+INSERT INTO card_tag VALUES (18, 'annie');
+
+-- card_id 19
+INSERT INTO card VALUES (19, 'proving grounds', '019', 'wuju bladesman-starter', 'legend', 'rare', NULL);
+INSERT INTO card_domain VALUES (19, 'calm');
+INSERT INTO card_domain VALUES (19, 'body');
+INSERT INTO card_tag VALUES (19, 'master yi');
+
+-- card_id 20
+INSERT INTO card VALUES (20, 'proving grounds', '020', 'highlander', 'signature spell', 'epic', 4);
+INSERT INTO card_domain VALUES (20, 'calm');
+INSERT INTO card_domain VALUES (20, 'body');
+INSERT INTO card_tag VALUES (20, 'master yi');
+
+-- card_id 21
+INSERT INTO card VALUES (21, 'proving grounds', '021', 'lady of luminosity-starter', 'legend', 'rare', NULL);
+INSERT INTO card_domain VALUES (21, 'mind');
+INSERT INTO card_domain VALUES (21, 'order');
+INSERT INTO card_tag VALUES (21, 'lux');
+
+-- card_id 22
+INSERT INTO card VALUES (22, 'proving grounds', '022', 'final spark', 'signature spell', 'epic', 8);
+INSERT INTO card_domain VALUES (22, 'mind');
+INSERT INTO card_domain VALUES (22, 'order');
+INSERT INTO card_tag VALUES (22, 'lux');
+
+-- card_id 23
+INSERT INTO card VALUES (23, 'proving grounds', '023', 'might of demacia-starter', 'legend', 'rare', NULL);
+INSERT INTO card_domain VALUES (23, 'body');
+INSERT INTO card_domain VALUES (23, 'order');
+INSERT INTO card_tag VALUES (23, 'garen');
+
+-- card_id 24
+INSERT INTO card VALUES (24, 'proving grounds', '024', 'decisive strike', 'signature spell', 'epic', 5);
+INSERT INTO card_domain VALUES (24, 'body');
+INSERT INTO card_domain VALUES (24, 'order');
+INSERT INTO card_tag VALUES (24, 'garen');
--- database/sql_schema_scripts/create_database_v0.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS deckbuilder;
-CREATE DATABASE deckbuilder;
-USE deckbuilder;
